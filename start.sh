e505d141d314ad4a828db5276741805b3767d509
ssainball <35214356+ssainball@users.noreply.github.com>
2020-01-21 17:07:54 +0900

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-count=`ps -ef | grep jormungandr | wc -l`
-echo $count
-
-if (( $count < 2 ))
-then
-  /usr/bin/nohup /root/jormungandr --genesis-block-hash $(cat /root/genesis-hash.txt) --config /root/config.yaml --secret /root/node_secret.yaml --log-level info &
-fi
